6c8198acc8da0a82c7e02b6d9be3f31e946f6a43
antonlabz <20198370+antonlabz@users.noreply.github.com>
2017-12-11 22:53:34 +1100

    fd

--- flairs.js
+++ flairs.js
@@ -1,12 +1,12 @@
 $(document).ready(function() {
   var folder = "flairs/";
-$("div").css("border", "3px solid red");
+
   $.ajax({
       url : folder,
       success: function (data) {
           $(data).find("a").attr("href", function (i, val) {
               if( val.match(/\.(jpe?g|png|gif)$/) ) {
-                  $("#content").append( "<img src='" + val +"'>" );
+                  $("body").append( "<img src='" + val +"'>" );
               }
           });
       }
