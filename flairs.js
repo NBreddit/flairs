d2334117b48128d9be9ae9190c5492a583b7a57e
antonlabz <20198370+antonlabz@users.noreply.github.com>
2017-12-11 22:00:44 +1100

    a

---
+++ flairs.js
@@ -0,0 +1,14 @@
+$(document).ready(function() {
+  var folder = "/flairs/";
+
+  $.ajax({
+      url : folder,
+      success: function (data) {
+          $(data).find("a").attr("href", function (i, val) {
+              if( val.match(/\.(jpe?g|png|gif)$/) ) {
+                  $("#content").append( "<img src='"+ val +"' class='flair'>" );
+              }
+          });
+      }
+  });
+});
