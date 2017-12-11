f50af39b9cdfad45f0d70bc8cf6010076319e56c
antonlabz <20198370+antonlabz@users.noreply.github.com>
2017-12-11 23:24:37 +1100

    dsa

--- flairs.js
+++ flairs.js
@@ -1,14 +1,21 @@
-$(document).ready(function() {
-  var folder = "flairs/";
+loadPage = function() {
 
-  $.ajax({
-      url : folder,
-      success: function (data) {
-          $(data).find("a").attr("href", function (i, val) {
-              if( val.match(/\.(jpe?g|png|gif)$/) ) {
-                  $("#content").append( "<img src='" + val +"'>" );
-              }
-          });
-      }
-  });
-});
+	//LOAD  FLAIRS
+	loadFlairs = function() {
+    var folder = "flairs/";
+
+    $.ajax({
+        url : folder,
+        success: function (data) {
+            $(data).find("a").attr("href", function (i, val) {
+                if( val.match(/\.(jpe?g|png|gif)$/) ) {
+                    $("#content").append( "<img src='" + val +"'>" );
+                }
+            });
+        }
+    });
+	}
+	loadFlairs();
+}
+
+document.addEventListener('DOMContentLoaded', loadPage, false);
