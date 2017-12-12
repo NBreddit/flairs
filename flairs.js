d67e4b0726f196097d237ad154ae8847d2182c56
antonlabz <20198370+antonlabz@users.noreply.github.com>
2017-12-12 13:05:54 +1100

    d

--- flairs.js
+++ flairs.js
@@ -1,14 +1,11 @@
 $(document).ready(function() {
-    var folder = "https://github.com/NBreddit/flairs/tree/master/icons";
-
-    $.ajax({
-        url : folder,
-        success: function (data) {
-            $(data).find("a").attr("href", function (i, val) {
-                if( val.match(/\.(jpe?g|png|gif)$/) ) {
-                    $("#content").append( "<img src='" + val +"'>" );
-                }
-            });
-        }
-    });
+  $.ajax({
+    url: "/repos/:NBreddit/:repo/contents/:icons",
+    jsonp: true,
+    method: "GET",
+    dataType: "json",
+    success: function(res) {
+      console.log(res)
+    }
+  });
 });
